ca8854c7dcb46956755c75f8e98120f508c713b0
Natalie <10840498@my.uvu.edu>
2024-04-14 19:10:02 -0600

    Watering behavior is working

--- UnityEventScripts/CompareBehaviour.cs
+++ UnityEventScripts/CompareBehaviour.cs
@@ -13,64 +13,53 @@ public class CompareBehaviour : MonoBehaviour
    public UnityEvent matchEvent, noMatchEvent, noMatchDelayedEvent, updateEvent, updateEventOne, updateEventTwo, updateEventThree, exitTriggerEvent;
    private bool isInside = false;
    private bool hasHandledTrigger = false;
+   
    private IEnumerator OnTriggerEnter(Collider other)
    {
       if (hasHandledTrigger) yield break; // Check if trigger has been handled already
       hasHandledTrigger = true; // Set trigger handling to true
       Debug.Log("Trigger");
-
       isInside = true;
       
-      Debug.Log("Trigger");
       var tempObj = other.GetComponent<IDContainerBehavior>();
       if (tempObj == null)
          yield break;
 
       var idOther = tempObj.idObj;
-      if (idOther == idObj)
+      if (idOther != idObj) yield break;
+      while (intObj2.value <= intObj.value && isInside == true)
       {
-         CompareValue();
+         if (update.value == 0 && isInside)
+         {
+            updateEvent.Invoke();
+         }
+
+         if (update.value == 1 && isInside)
+         {
+            updateEventOne.Invoke();
+         }
+
+         if (update.value == 2 && isInside)
+         {
+            updateEventTwo.Invoke();
+         }
+
+         if (update.value == 3 && isInside)
+         {
+            updateEventThree.Invoke();
+         }
+
+         yield return new WaitForSeconds(2.0f);
+         Debug.Log("Delay");
+         update.value++;
+         noMatchDelayedEvent.Invoke();
+         Debug.Log("No Match");
       }
    }
-
-private IEnumerator CompareValue()
-{
-   while (intObj2.value != intObj.value && intObj2.value < intObj.value && isInside == true)
-   {
-      if (update.value == 0 && isInside)
-      {
-         updateEvent.Invoke();
-      }
-
-      else if (update.value == 1 && isInside)
-      {
-         updateEventOne.Invoke();
-      }
-
-      else if (update.value == 2 && isInside)
-      {
-         updateEventTwo.Invoke();
-      }
-
-      else if (update.value == 3 && isInside)
-      {
-         updateEventThree.Invoke();
-      }
-
-      yield return new WaitForSeconds(3.0f);
-      Debug.Log("Delay");
-      update.value++;
-      noMatchDelayedEvent.Invoke();
-      Debug.Log("No Match");
-   }
-}
-
-private void OnTriggerExit(Collider other)
+   private void OnTriggerExit(Collider other)
    {
       isInside = false;
       hasHandledTrigger = false; // Reset trigger handling when exiting trigger
       exitTriggerEvent.Invoke();
    }
-
-  
 }
