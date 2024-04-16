7ee72059fc752dc2fc39be4ae3e280248d501429
Natalie <10840498@my.uvu.edu>
2024-04-16 12:21:32 -0600

    Added Trigger to remove objects

--- UnityEventScripts/RemoveObjects.cs
+++ UnityEventScripts/RemoveObjects.cs
@@ -5,8 +5,30 @@ using UnityEngine;
 
 public class RemoveObjects : MonoBehaviour
 {
+    public bool isTriggered = false;
+    private bool isInside = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other);
+        isInside = true;
+        if (isInside == true && isTriggered)
+        {
+            Destroy(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInside = false;
+    }
+    
+    public void setTriggerTrue()
+    {
+        isTriggered = true;
+    }
+    
+    public void setTriggerFalse()
+    {
+        isTriggered = false;
     }
 }
