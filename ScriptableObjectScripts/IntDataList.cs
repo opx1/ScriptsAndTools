fad81371885b3193546bd78eff6f21d282f64cab
Owynn Armstrong <10932343@uvu.edu>
2024-12-11 13:10:06 -0700

    added scripts

---
+++ ScriptableObjectScripts/IntDataList.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntDataList : ScriptableObject
+{
+    public List<IntData> IntdataList;
+    public IntData currentIntData;
+}
